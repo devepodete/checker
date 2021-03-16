877891eca8c51fdbfaee842d6ac717bd6bcf2a6f
Maksim Cheremisinov <54975008+devepodete@users.noreply.github.com>
2021-03-16 13:49:53 +0300

    Create BuilderPaths.java

---
+++ src/Logging/BuilderPaths.java
@@ -0,0 +1,10 @@
+package Logging;
+
+public class BuilderPaths {
+    public static final String javacPath = "/usr/lib/jvm/jdk-15.0.2/bin/javac";
+    public static final String buildClassPath = "/home/zero/Projects/IdeaProjects/Checker/out/production";
+    public static final String CheckerClassName = "Checker";
+    public static final String CheckerClassPath = String.join("/", buildClassPath, CheckerClassName);
+
+
+}
